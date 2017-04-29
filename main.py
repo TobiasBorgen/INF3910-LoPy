0db690ada123edee67683cc1bdd7aa14124f2924
Petter Tunstad <Pettertunstad@gmail.com>
2017-04-29 15:02:18 +0200

    LoPy sending wind and direction over LoraWAN

--- main.py
+++ main.py
@@ -2,27 +2,39 @@ from config import dev_eui, app_eui, app_key
 from lora import LORA
 from davis7911 import DAVIS7911
 import time
+##from si7021 import SI7021
 
 def setup():
-  global n, sensor_davis
+  global n, sensor_davis, sensor_temp
 
   # Connect to LoRaWAN
-  #n = LORA()
-  #n.connect(dev_eui, app_eui, app_key)
+  n = LORA()
+  n.connect(dev_eui, app_eui, app_key)
 
   # Connect Davis sensor
   try:
+    ##sensor_temp = SI7021()
     sensor_davis = DAVIS7911()
   except Exception as e:
     print('Error: ', e)
 
-def format(wind_speed):
+def format_s(wind_speed):
   windspeedstr = ''
   if wind_speed >= 10:
     windspeedstr = str(wind_speed) + '.00'
   else:
     windspeedstr = '0' + str(wind_speed) + '.00'
-  return '0' + '005.00' + windspeedstr + '220.00'
+  return windspeedstr
+
+def format_d(wind_dir):
+  winddirstr = ''
+  if wind_dir > 360:
+    winddirstr = '360'
+  elif wind_dir < 0:
+    winddirstr = '0'
+  else:
+    winddirstr = str(wind_dir)
+  return winddirstr
 
 if __name__ == '__main__':
   setup()
@@ -30,8 +42,13 @@ if __name__ == '__main__':
   while True:
     
     # Send payload
-    #data = format(sensor_davis.get_windspeed())
-    #response = n.send(data)
-    tmp = sensor_davis.get_dir()
-    print(tmp)
-    time.sleep(0.1)  
+    speed = format_s(sensor_davis.get_windspeed())
+    direction = format_d(sensor_davis.get_dir())
+     ##print(direction)
+    data = '0' + '018.00' + speed + direction
+    response = n.send(data)
+    ##hum = sensor_temp.getRH()
+    ##temp = sensor_temp.readTemp()
+    ##print(temp)
+   
+    time.sleep(60)  
