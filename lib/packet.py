a790a9bfcd857d9015166c0946924a2e0c8e60ef
Pwntus <pontus.edvard@gmail.com>
2017-05-04 11:14:27 +0200

    Fix bugs

--- lib/packet.py
+++ lib/packet.py
@@ -4,11 +4,33 @@ class Packet(object):
   t = None  # Temperature   6b
   s = None  # Speed         5b
   d = None  # Direction     6b
+  
+  def zfill(self, string, num):
+    """
+    Generic zfill.
+    """
+    
+    # Find position of decimal point
+    try:
+      pos = string.index('.')
+    except Exception as e:
+      pos = 0
+    
+    missing = num - pos
+    if missing > 0:
+      string = ('0' * missing) + str(string)
+      
+    return float(string)
     
   def reset(self):
     self.t = None
     self.s = None
     self.d = None
+    
+  def clean(self):
+    self.t = '' if self.t is None else self.t
+    self.s = '' if self.s is None else self.s
+    self.d = '' if self.d is None else self.d
 
   def set_t(self, t):
     """
@@ -30,7 +52,8 @@ class Packet(object):
     # 1. Convert s to a string with 2 decimals.
     # 2. Pad the string to match a total of 4 characters
     #    with zfill().
-    self.s = '%.2f'.zfill(4) % s
+    s = self.zfill(s, 2)
+    self.s = '%.2f' % s
 
   def set_d(self, d):
     """
@@ -38,7 +61,8 @@ class Packet(object):
     E.g. 145.87
          003.00
     """
-    self.d = '%.2f'.zfill(5) % d
+    d = self.zfill(d, 3)
+    self.d = '%.2f' % d
 
   def get(self):
     t = '1' if self.t is not None else '0'
@@ -48,9 +72,12 @@ class Packet(object):
     # Convert binary string to decimal.
     # E.g. 101 => 5, wind speed excluded.
     f = str(int(t + s + d, 2))
+    
+    # Clean values (None > xxx.xx)
+    self.clean()
 
     # Store packet and reset
-    packet = self.f + self.t + self.s + self.d
+    packet = f + self.t + self.s + self.d
     self.reset()
 
     return packet
