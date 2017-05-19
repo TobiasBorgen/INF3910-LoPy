fcef360d271fe6c6e14cb5d704fa58ffd0f175e3
Petter Tunstad <Pettertunstad@gmail.com>
2017-05-19 16:38:00 +0200

    LoPy now sets time and format from frontend response

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from packet import Packet
 from si7021 import SI7021
 from time import sleep
 from machine import deepsleep
+import ujson
+
 
 def setup():
-  global n, sensor_davis, sensor_temp, packet
+  global n, sensor_davis, sensor_temp, packet, sleep_time, state
+
+  # Initial state and sleeptime
+  state = 7
+  sleep_time = 10
   
   # Init packet object
   packet = Packet()
@@ -23,26 +29,48 @@ def setup():
   except Exception as e:
     print('Error: ', e)
 
+def handle_response(data):
+  global sleep_time, state
+
+  try:
+    response = ujson.loads(data)[0]
+  except ValueError as e:
+    print("Exception occured while parsing response",  e)
+    return
+  try:
+    state = int(response['f'])
+  except:
+    print("No new format to set")
+  try:
+    sleep_time = int(response['u'])
+  except:
+    print("No new update interval")
+
 if __name__ == '__main__':
 
   # Setup network & sensors
   setup()
 
   while True:
-    
+    sleep(sleep_time) # 10s
     # Measure
     try:
-      #packet.set_t(sensor_temp.getRH())
-      packet.set_t(sensor_temp.getTemp())
-      packet.set_s(sensor_davis.get_windspeed())
-      packet.set_d(sensor_davis.get_dir())
+      #packet.set_t(sensor_temp.getRH()) 
+      if(state == 7 or state == 6 or state == 5 or state == 4):
+        packet.set_t(sensor_temp.getTemp())
+      if(state == 7 or state == 6 or state == 3 or state == 2):
+        packet.set_s(sensor_davis.get_windspeed())
+      if(state == 7 or state == 5 or state == 3 or state == 1):
+        packet.set_d(sensor_davis.get_dir())
     except Exception as e:
       print('Measure error: ', e)
 
 
     # Send packet
     response = n.send(packet.get())
+    if (len(response) > 0):
+        handle_response(response)
+
     
-    sleep(10) # 10s
     #deepsleep(10000) # 10s
 
